a5632e35db4c79b2abafb54da5fe519056f4b265
plantz <f.kauker@gmail.com>
2020-05-13 18:26:56 -0700

    sketch out class

---
+++ globeloc.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+"""Create the globeloc API and helpers
+"""
+
+
+
+__author__ = "Fabion Kauker"
+__copyright__ = "Copyright 2020, globeloc"
+__credits__ = []
+__license__ = "Super Secret Squirrel"
+__version__ = "0.0.1"
+__maintainer__ = ""
+__email__ = "support@globeloc.com"
+__status__ = "Protype"
+
+
+
+class GlobeLoc:
+	def __init__(self):
+		api_key = None
+		data_sets = []
+		data_arrays = []
+
+	def __get_item__(self, item):
+		return DataArray(self)
+
+	def connect(self, api_key):
+		pass
+
+	def load(self, unique_id):
+		pass
+
+	def save(self, my_array):
+		return "unique_id"
+
+	def parse(self, filename):
+		pass
+
+
+class DataArray:
+	def __init__(self, globeloc):
+		pass
+
+	def __get_item__(self, item):
+		pass
+
